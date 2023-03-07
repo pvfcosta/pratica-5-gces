4c97fc4e40aba90ef25caaf852eeef089ea6723e
pvfcosta <pedrovfc11@gmail.com>
2023-03-07 20:19:08 -0300

    Initial commit javascript

--- main-test.py
+++
@@ -1,9 +0,0 @@
-from main import Add
-
-def TestAdd():
-        assert Add(2,3) == 5
-        assert Add(5,5) == 10
-        print("Add Function works correctly")
-
-if __name__ == '__main__':
-        TestAdd()
---
+++ main.js
@@ -0,0 +1,7 @@
+function Add (a, b) {
+    return a + b;
+}
+
+function SayHello() {
+    console.log("sup world form srcmake");
+}
